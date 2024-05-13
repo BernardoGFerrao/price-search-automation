acad5aa35c185ef3c1c68d46f80f969970bdc821
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-12 23:43:36 -0300

    starting the search on google shopping

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-#Criar um navegador
+#Bibliotecas
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import time
-
+#Criação navegador:
 navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-link = "https://www.buscape.com.br"
-navegador.get(link)
 
 #Importar/Visualizar a base de dados
 produtos_df = pd.read_excel('buscas.xlsx')
 print(produtos_df)
-#Para cada item dentro da base de dados:
-#   - Procurar o produto no google shooping
-#       - Verificar se algum dos produtos está dentro da faixa de preços
-#   - Procurar o produto no buscapé
-#       - Verificar se algum dos produtos está dentro da faixa de preços
+
+##GOOGLE:
+#Entrar no google
+link = "https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwjUn-eMy4mGAxV5LbkGHQFiAgsQPAgJ"
+navegador.get(link)
+#Pesquisar pelo produto
+produto = 'iphone 12 64gb'
+barra_pesquisa = navegador.find_element('xpath', '//*[@id="APjFqb"]')
+barra_pesquisa.send_keys(produto, Keys.ENTER)
+#Entrar na aba shopping
+
+#Pegar as informações do produto
+
+
+##BUSCAPÉ:
+
 
 #Salvar as ofertas boas em um dataframe
 
@@ -25,5 +35,5 @@ print(produtos_df)
 #Enviar por email o resultado da tabela
 
 # Fecha o navegador
-time.sleep(10)
+time.sleep(100)
 navegador.quit()
