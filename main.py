578c367d9657326f3acfe8b2bfa9779a8bb6893f
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-05-13 02:47:51 -0300

    Finishing product search function on Google Shopping(fix)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ navegador = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 #Importar/Visualizar a base de dados
 produtos_df = pd.read_excel('buscas.xlsx')
 print(produtos_df)
-def buscar_google(produto, termos_banidos, preco_minimo, preco_maximo):
+def buscar_google(navegador, produto, termos_banidos, preco_minimo, preco_maximo):
     ##GOOGLE:
     #Entrar no google
     link = "https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwjUn-eMy4mGAxV5LbkGHQFiAgsQPAgJ"
@@ -32,7 +32,7 @@ def buscar_google(produto, termos_banidos, preco_minimo, preco_maximo):
     barra_pesquisa.send_keys(produto, Keys.ENTER)
 
     #Entrar na aba shopping
-    time.sleep(3)
+    time.sleep(5)
     elemento = navegador.find_element('xpath', '//*[@id="hdtb-sc"]/div/div/div[1]/div/div[2]/a')
     link = elemento.get_attribute('href')  # Obter o link do elemento
     navegador.get(link)  # Acessar o link
@@ -41,7 +41,6 @@ def buscar_google(produto, termos_banidos, preco_minimo, preco_maximo):
     lista_resultados = navegador.find_elements('class name', 'sh-dgr__content')
     #Para cada resultado -> 1 nome, 1 link e 1 preco
     lista_produtos = []
-
     for resultado in lista_resultados:
         nome = resultado.find_element('class name', 'tAxDx').text
         nome = nome.lower()
@@ -67,7 +66,6 @@ def buscar_google(produto, termos_banidos, preco_minimo, preco_maximo):
                 preco = float(match.group())
             else:
                 continue
-
             #Link
             elemento_referencia_filho = resultado.find_element('class name', 'bONr3b')
             elemento_pai = elemento_referencia_filho.find_element('xpath', '..')
@@ -76,8 +74,9 @@ def buscar_google(produto, termos_banidos, preco_minimo, preco_maximo):
             #Verificação final:
             if preco <= preco_maximo and preco >= preco_minimo:
                 lista_produtos.append((nome, preco, link))
-
-
+    return lista_produtos
+lista_produtos_google = buscar_google(navegador, 'iphone 12 64 gb', 'mini watch', 3000, 5000 )
+print(lista_produtos_google)
 ##BUSCAPÉ:
 
 
